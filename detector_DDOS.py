5c3bacea7dd4919a3a0915fd5fba07c4ac3a0eb4
ZvonarevEgor <zvonarev96@mail.ru>
2019-07-12 17:22:37 +0300

    refactor frequent_element

--- detector_DDOS.py
+++ detector_DDOS.py
@@ -1,6 +1,3 @@
-from collections import Counter
-
-
 # Get the conditions and fill the array.
 def input_info():
     a = []
@@ -12,63 +9,23 @@ def input_info():
     return a, n, m, k
 
 
-# Pass through the array checking the M elements for shift.
-def several_indexes(m, this_index):
-    list_with_indexes = []
-    for i in range(m):
-        list_with_indexes.append(i + this_index)
-    return list_with_indexes
-
-
-# Get bad addresses from M elements.
-def get_bad_ip(m_elements, k):
-    bad_ip = []
-    result = Counter(m_elements).most_common(len(m_elements))
-    for ip in result:
-        if ip[1] >= k:
-            bad_ip.append(ip[0])
+def main(array, m, n, k):
+    bad_ip = set()
+    for i, ip in enumerate(array):
+        if ip in bad_ip:
+            continue
+        count = 1
+        for idx in range(i + 1, min(i + m, len(array))):
+            if ip == array[idx]:
+                count += 1
+        if count >= k:
+            bad_ip.add(ip)
+    bad_ip = sorted(bad_ip)
     return bad_ip
 
 
-# Addresses in this list can be repeated.
-def get_m_elements(a, n, m, this_index):
-    m_elements = []
-    list_with_indexes = several_indexes(m, this_index)
-    for index in list_with_indexes:
-        if index < n:
-            m_elements.append(a[index])
-    return m_elements
-
-
-
-# Get bad addresses from the whole array, eliminating repetition.
-def get_final_list(a, n, m, k):
-    final_ip_list = []
-    for i in range(len(a)):
-        m_elements = get_m_elements(a, n, m, i)
-        list_with_bad_ip = (get_bad_ip(m_elements, k))
-        for ip in list_with_bad_ip:
-            if ip not in final_ip_list:
-                final_ip_list.append(ip)
-    return final_ip_list
-
-def output_info(ip_list):
-    output = ''
-    for ip in ip_list:
-        output += '{}\n'.format(ip)
-    return output
-
-
-def main():
-    a, n, m, k = input_info()
-    bad_ip_list = get_final_list(a, n, m, k)
-    bad_ip_list.sort(key=lambda element: int(element[-1]))
-    output_str = output_info(bad_ip_list)
-    return output_str
-
-
 if __name__ == '__main__':
-    print(main())
-
-
+    a, n, m, k = input_info()
+    bad_ip = main(a, n, m, k)
+    print(bad_ip, sep='\n')
 
